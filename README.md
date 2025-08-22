694f96008630a10e023bbae1559174e8b2ab26ac
Ricardo Pires <ricardo0605@gmail.com>
2025-08-22 11:09:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cloud fundamentals - AWS services for csharp developers.
+
+**AWS Secrets Manager**
+\
+\
+\
+This is one of many projects to following allong a Dometrain course. 
+This course was fully sponsord by AWS, meaning you can enroll it for free and the course is yours to keep and complete.
+\
+<sub>[Check it out here](https://dometrain.com/course/cloud-fundamentals-aws-services-for-c-developers)</sub>
---
+++ SecretsManager.App/Program.cs
@@ -0,0 +1,32 @@
+﻿using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
+
+namespace SecretsManager.App;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(30));
+        var client = new AmazonSecretsManagerClient(RegionEndpoint.USEast1);
+
+        var request = new GetSecretValueRequest
+        {
+            SecretId = "ApiKey2"
+        };
+
+        try
+        {
+            Console.WriteLine($"Getting the secret at {DateTime.Now}");
+            
+            var response = await client.GetSecretValueAsync(request, cts.Token);
+
+            Console.WriteLine($"Secret retrieved: {response.SecretString} at {DateTime.Now}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Error retrieving secret: " + e.Message);
+        }
+    }
+}
