a5ae40c3b9ea61afc1c771c35ffb415850bbbf0b
Ricardo Pires <ricardo0605@gmail.com>
2025-08-25 09:45:08 -0300

    Dealing with secret rotations

--- Weather.Api/Program.cs
+++ Weather.Api/Program.cs
@@ -12,7 +12,16 @@ builder.Configuration.AddSecretsManager(configurator: options =>
     options.KeyGenerator = (_, s) => s
         .Replace($"{env}_{appName}_", string.Empty)
         .Replace("__", ":");
+
+
+    // This will be an interval that our service will poll for a secret update.
+    // I'm gonna to leave this short for study purposes but ultimately you should
+    // only be doing that in an interval that makes sense because we are paying per
+    // request on secret manager.
+    options.PollingInterval = TimeSpan.FromSeconds(10);
 });
+
+
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
